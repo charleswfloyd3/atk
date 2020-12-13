f3b33198f1150ef222b451113bdd62346f368525
Charles Floyd <charleswfloyd3@gmail.com>
2020-12-12 23:39:15 -0800

    cancel

--- src/countdown/countdown.js
+++ src/countdown/countdown.js
@@ -8,9 +8,9 @@ function Countdown(){
 
  return(
     <div class="secondwebpage">
-        <h1 className="signup">Sign up to recieve updates!</h1>
+        {/* <h1 className="signup">Sign up to recieve updates!</h1>
         <input className="email" type="email" placeholder="enter your email address..."></input>
-        <br></br><button className="signupbtn" onClick={Signup}>Sign Up</button>
+        <br></br><button className="signupbtn" onClick={Signup}>Sign Up</button> */}
 
     </div>    
  )
