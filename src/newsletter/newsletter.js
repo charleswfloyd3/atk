93e8bb86c2d1ce440cabf65b2a210e838b29104f
Charles Floyd <charleswfloyd3@gmail.com>
2021-01-05 18:20:14 -0800

    making the newsletter validate more emails more accurately

--- src/newsletter/newsletter.js
+++ src/newsletter/newsletter.js
@@ -2,23 +2,45 @@ import React from 'react'
 import './newsletter.css'
 
 function Newsletter(){
+    let name = React.useRef(null)
+    let email = React.useRef(null)
+    let mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    let errormessage = "";
+function eventHandler(e){
+    e.preventDefault()
 
+    let userData =  {name: name.current.value,
+            email: email.current.value}
+    if(userData.email.match(mailformat) && userData.name.length > 1){
+   
+    console.log(userData)
+    }
+    else if(userData.name.length < 1){
+        return errormessage = "please enter a valid name"
+    }
+    else{
+        return errormessage = "please enter a valid email address"
+    }    
+}
+console.log(name)
  return(
+
     <div class="secondwebpage">
         <form action="action_page.php">
                 <div class="container">
-                    <h2>Subscribe to our Newsletter!</h2>
+                    <h1>Join our Newsletter!</h1>
                 </div>
                 <div class="container" >
-                    <input type="text" placeholder="Name" name="name" required />
-                    <input type="text" placeholder="Email address" name="mail" required />
+                    <input type="text" placeholder="Name" ref={name} required />
+                    <input type="text" placeholder="Email address" ref={email}required />
                     <label>
-                        <input type="checkbox"  name="subscribe" />Recieve Updates
+                    <input type="checkbox"  name="subscribe" />Recieve Updates
                     </label>
                 </div>
                 
                 <div class="container">
-                    <input type="submit" value="Subscribe" />
+                    <h4>{errormessage}</h4>
+                    <input type="submit" onClick={eventHandler} value="Subscribe" />
                 </div>
         </form>
     </div>    
