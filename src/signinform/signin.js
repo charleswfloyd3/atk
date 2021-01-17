5f33bdec9141ad0b780627a07242e30e6d44c0df
Charles Floyd <charleswfloyd3@gmail.com>
2021-01-17 13:15:26 -0800

    added validation for correct user input

--- src/signinform/signin.js
+++ src/signinform/signin.js
@@ -8,6 +8,8 @@ function Signin() {
   let email = React.useRef(null)
   let mailformat = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{3}$/;
   let [classStatus, setClassStatus] = useState(true);
+  let [classStatusbtn, setClassStatusbtn] = useState(false);
+
   let setClassStatuss = () =>{
     if(classStatus === false){
       setClassStatus(true)
@@ -21,10 +23,19 @@ function Signin() {
     e.preventDefault()
     if(email.current.value.match(mailformat)){
       setClassStatuss()
+    }
+    else{
+    } 
+  }
+
+  let buttonstatus = (e) => {
+    e.preventDefault()
+    if(email.current.value.match(mailformat)){
+      setClassStatusbtn(true)
       console.log("hey")
     }
     else{
-
+      setClassStatusbtn(false)
     } 
   }
   return (
@@ -33,10 +44,10 @@ function Signin() {
     <form className={classStatus ? "signinformemail" : "signindivhidden"}>
         <h1 className="companyname">A T K</h1>
         <p className="signintitle">Sign in</p>
-        <input className="inputfieldemail" placeholder='Enter email' ref={email}></input>
+        <input className="inputfieldemail" placeholder='Enter email' ref={email} onChange={buttonstatus}></input>
         <div className="bottomofform">
         <p className='createaccount'><Link style={{ color: 'inherit', textDecoration: 'inherit'}} to="/signup">Create account</Link></p>
-        <button className="nextbutton" onClick={Next}>Next</button>
+        <button className={classStatusbtn ? "nextbutton": "nextbutton3error"}onClick={Next}>Next</button>
         </div>
     </form>
      <form className={classStatus ? "signindivhidden" : "signinformpassword"}>
@@ -46,7 +57,7 @@ function Signin() {
         <p className="gobackbutton" onClick={setClassStatuss}>Go back</p>
         <div className="bottomofform">
         <p className='createaccountfinal'><Link style={{ color: 'inherit', textDecoration: 'inherit'}} to="/signup">Create account</Link></p>
-        <button className="nextbuttonfinal">Sign In</button>
+        <button className="nextbuttonfinal" >Sign In</button>
         </div>
      </form>
     </div>
--- src/singupform/signup.js
+++ src/singupform/signup.js
@@ -1,9 +1,23 @@
-import react from "react";
+import React,{useState} from 'react'
 import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
 import NavbarForms from "../navbarforforms/navbarforms";
 import "./signup.css";
 
 function Signup() {
+  let [classStatus, setClassStatuss] = useState(false);
+  let mailformat = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{3}$/;
+  let email = React.useRef(null)
+
+  let buttonstatus = (e) => {
+    e.preventDefault()
+    if(email.current.value.match(mailformat)){
+      setClassStatuss(true)
+      console.log("hey")
+    }
+    else{
+      setClassStatuss(false)
+    } 
+  }
   return (
     <div className="signindiv">
     <NavbarForms />
@@ -13,10 +27,10 @@ function Signup() {
       <p className="or">or</p>
       <p className="browseas">browse as </p>
       <button className="guestbutton">GUEST</button>
-      <input className="inputfieldemail" placeholder="Enter email"></input>
+      <input className="inputfieldemail" ref={email} placeholder="Enter email" onChange={buttonstatus}></input>
       <div className="bottomofform">
         <p className="alreadyhaveanaccount"><Link style={{ color: 'inherit', textDecoration: 'inherit'}} to="/login">Already have an account?</Link></p>
-        <button className="nextbutton2">Next</button>
+        <button className={classStatus ? "nextbutton2": "nextbutton2error"} >Next</button>
       </div>
     </form>
     {/* <form className="signupformpassword">
